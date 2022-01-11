77eadae91b5e60d6ac806928cc2d951d8bfacd5e
GeorgiiVoyakin <voyaking@gmail.com>
2022-01-12 01:25:56 +0300

    Used fact that tree contains 10 elements

--- main.go
+++ main.go
@@ -22,26 +22,29 @@ func Walk(t *tree.Tree, ch chan int) {
 // Same determines whether the trees
 // t1 and t2 contain the same values.
 func Same(t1, t2 *tree.Tree) bool {
-	tree1 := make([]int, 5)
-	tree2 := make([]int, 5)
+	var tree1 []int
+	var tree2 []int
 	ch1 := make(chan int)
 	ch2 := make(chan int)
 	go Walk(t1, ch1)
 	go Walk(t2, ch2)
 
-	for {
-		select {
-		case x := <-ch1:
-			tree1 = append(tree1, x)
-		case x := <-ch2:
-			tree2 = append(tree2, x)
-		default:
-			break
-		}
+	for i := 0; i < 10; i++ {
+		x := <-ch1
+		tree1 = append(tree1, x)
+		x = <-ch2
+		tree2 = append(tree2, x)
+
 	}
 
-	sort.Slice(tree1, func(i, j int) bool { return i < j })
-	sort.Slice(tree2, func(i, j int) bool { return i < j })
+	fmt.Println(tree1)
+	fmt.Println(tree2)
+	sort.Slice(tree1, func(i, j int) bool { return tree1[i] < tree1[j] })
+	sort.Slice(tree2, func(i, j int) bool { return tree2[i] < tree2[j] })
+
+	fmt.Println(tree1)
+	fmt.Println(tree2)
+
 	if len(tree1) != len(tree2) {
 		return false
 	}
@@ -55,15 +58,5 @@ func Same(t1, t2 *tree.Tree) bool {
 }
 
 func main() {
-	// Same(tree.New(1), tree.New(1))
-	ch := make(chan int)
-	go Walk(tree.New(1), ch)
-	for {
-		select {
-		case x := <-ch:
-			fmt.Println(x)
-		default:
-			break
-		}
-	}
+	fmt.Println(Same(tree.New(2), tree.New(2)))
 }
